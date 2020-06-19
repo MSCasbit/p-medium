e76902649c816a22bb6aa5bc07f83d53fc4e9992
mauricio <castillo_smauricio292@outlook.com>
2020-06-19 17:36:13 -0500

    :construction_worker: [ADD] se agrego comienzo de JS y formulario

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var post = [
+    {
+        imgUrl:"",
+        name:"title post",
+        description:"",
+        autor:"",
+        section:"",
+        date:"",
+        readTime:"",
+        type: "",
+}
+
+]
+//post recientes no lleva descripción la parte del date 
+const primePost=()=>{
+    console.log (primePost)
+    let getBoard = $("#newPost")      
+    }
+
+
+    const post = {
+        title: "",
+        type: "",
+        content: "",
+        author: "",
+    }
