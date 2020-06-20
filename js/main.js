36cd1610f2f4e5cf5f2cf6a3ee74407e6bb77ab8
mauricio <castillo_smauricio292@outlook.com>
2020-06-20 14:48:35 -0500

    :bug: [FIX] arreglar errores y añadir modal a newPost con formulario

--- js/main.js
+++ js/main.js
@@ -11,16 +11,35 @@ var post = [
 }
 
 ]
-//post recientes no lleva descripción la parte del date 
-const primePost=()=>{
-    console.log (primePost)
-    let getBoard = $("#newPost")      
-    }
 
+ const loadPage=()=>{
+     $("#allContent").load("newPost.html",printForm)
+ }
+const printForm=()=>{
+    $("#allContent").append(`
+    <div class="container">
+    <div class="row">
+      <div class="col-12 col-md-6">
+    <form>
+    <div class="form-group">
+      <label for="autor">autor</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">      
+    </div>
+    <div class="form-group">
+      <label for="autor">autor</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">      
+    </div>
+    <div class="form-group">
+      <label for="autor">autor</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">      
+    </div>
+    <div class="form-group">
+      <label for="descripcion">descripcion</label>
+      <textarea type="text" class="form-control"></textarea>
+      </div>
+      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#confirmModal">Subir post</button>
+      </div>
+    </div>
+    </div>`)
+}
 
-    const post = {
-        title: "",
-        type: "",
-        content: "",
-        author: "",
-    }
