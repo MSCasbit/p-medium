c33ff60d811eb1a0162ab5db88eb18abe9ecdd06
mauricio <castillo_smauricio292@outlook.com>
2020-06-20 19:20:43 -0500

    :contruction_worker: [ADD] infinty scroll on JS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var post = [
+/*var post = [
     {
         imgUrl:"",
         name:"title post",
@@ -10,36 +10,57 @@ var post = [
         type: "",
 }
 
-]
+]*/
+var viewportHeight = window.innerHeight
 
- const loadPage=()=>{
-     $("#allContent").load("newPost.html",printForm)
- }
-const printForm=()=>{
-    $("#allContent").append(`
-    <div class="container">
-    <div class="row">
-      <div class="col-12 col-md-6">
-    <form>
-    <div class="form-group">
-      <label for="autor">autor</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">      
-    </div>
-    <div class="form-group">
-      <label for="autor">autor</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">      
-    </div>
-    <div class="form-group">
-      <label for="autor">autor</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">      
-    </div>
-    <div class="form-group">
-      <label for="descripcion">descripcion</label>
-      <textarea type="text" class="form-control"></textarea>
-      </div>
-      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#confirmModal">Subir post</button>
-      </div>
-    </div>
-    </div>`)
-}
+ window.addEventListener("scroll", (event)=>{     
+    if( $(window).scrollTop() > $(document).height() - $(window).height() -100 ) {
+        loadMorePost()
+        
+    }
+})
 
+const loadMorePost =()=>{
+    //$("#containeer").append()
+    console.log("lo que quieras")
+    $.ajax({
+        url: "js/posts.json",        
+        success: function(data){
+            data.map(post=>{
+                $("#containeer").append(` <div id="second-card" class="card mb-3">
+                <div class="row no-gutters d-flex align-items-center  flex-row-reverse">
+                    <div class="col-4">
+                        <img src="https://d2kdyl1ietngbg.cloudfront.net/ProductThumbnails/yGtPHJ-683765271504_1.png"
+                            class="card-img mb-3 w-75" alt="">
+                    </div>
+                    <div class="col-8">
+                        <div class="card-body pr-0 pt-0">
+                            <h5 class="card-title font-weight-bold">${post.title}</h5>
+                            <div class="btn-group2 d-flex align-items-center justify-content-between">
+                                <div>
+                                    <p class="text muted">The Important Of Art Pictures, Try It. This weekend was a
+                                        bit different, though. </p>
+                                    <a class="text-dark user" href="https://medium.com/">veccilin</a>
+                                    <span class="text-dark user">in</span>
+                                    <a class="text-dark user" href="https://medium.com/">PaintLove</a>
+                                    <br><time class=" text-muted user">a few hours
+                                        ago</time><span class="text-muted user"> <img width="6px"
+                                            src="images/primitive-dot (2).svg" alt=""> 3 min
+                                        read </span><span> <img width="8px" src="images/star (3).svg" alt="">
+                                    </span>
+                                </div>
+                                <div class="d-flex mr-2">
+                                    <img class="menu" width="23px" src="images/guardar1.svg" alt="guardar">
+                                    <img class="menu" width="20px" src="images/more_horiz-24px (1).svg" alt="dotts">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`)
+            })
+        },
+        dataType: "json",
+        cache: false
+      });
+    }
