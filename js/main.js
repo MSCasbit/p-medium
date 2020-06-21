3f94343d43d8baf9a96e7a6c5b75e7bf842739a1
Nitzeli <ntzeli_alejandra@hotmail.com>
2020-06-20 19:29:38 -0500

    scroll Navbar

--- js/main.js
+++ js/main.js
@@ -4,42 +4,42 @@ var post = [
         name:"title post",
         description:"",
         autor:"",
-        section:"",
+        category:"",
         date:"",
         readTime:"",
-        type: "",
 }
 
 ]
 
- const loadPage=()=>{
-     $("#allContent").load("newPost.html",printForm)
- }
-const printForm=()=>{
-    $("#allContent").append(`
-    <div class="container">
-    <div class="row">
-      <div class="col-12 col-md-6">
-    <form>
-    <div class="form-group">
-      <label for="autor">autor</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">      
-    </div>
-    <div class="form-group">
-      <label for="autor">autor</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">      
-    </div>
-    <div class="form-group">
-      <label for="autor">autor</label>
-      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">      
-    </div>
-    <div class="form-group">
-      <label for="descripcion">descripcion</label>
-      <textarea type="text" class="form-control"></textarea>
-      </div>
-      <button type="button" class="btn btn-success" data-toggle="modal" data-target="#confirmModal">Subir post</button>
-      </div>
-    </div>
-    </div>`)
+
+const createPost = ()=>{
+    $( "#savePost" ).click(function( event ) {
+        console.log( $("form" ).serializeArray() );
+        event.preventDefault();
+      });
+}
+/*
+$("#savePost").click(() => {
+    $("#exampleModal").modal("hide");
+});
+      
+*/
+const loadPage = () =>{
+    $('#formPost').load('newPost.html #post-form')
 }
 
+// navbar animada 
+$("#horizon-prev").click(function(){
+    event.preventDefault();
+    $(".lix").animate({
+        scrollLeft: "-=100"
+      }, "slow");
+    });
+
+
+$("#horizon-next").click(function() {
+    event.preventDefault();
+    $(".lix").animate({
+     scrollLeft: "+=100"
+    }, "slow");
+  });
