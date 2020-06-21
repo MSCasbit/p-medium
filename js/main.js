81e38c67619956971ecf41a104e2c0dc18011552
mauricio <castillo_smauricio292@outlook.com>
2020-06-21 17:59:08 -0500

    creacion de cards

--- js/main.js
+++ js/main.js
@@ -20,22 +20,165 @@ window.addEventListener("scroll", (event) => {
 
     }
 })
+const loadMorePost =()=>{
+    //$("#containeer").append()
+    console.log("lo que quieras")
+    $.ajax({
+        url: "js/posts.json",        
+        success: function(data){
+            data.map(post=>{
+                $("#containeer").append(` <div id="second-card" class="card mb-3">
+                <div class="row no-gutters d-flex align-items-center  flex-row-reverse">
+                    <div class="col-4">
+                        <img src="https://d2kdyl1ietngbg.cloudfront.net/ProductThumbnails/yGtPHJ-683765271504_1.png"
+                            class="card-img mb-3 w-75" alt="">
+                    </div>
+                    <div class="col-8">
+                        <div class="card-body pr-0 pt-0">
+                            <h5 class="card-title font-weight-bold">${post.title}</h5>
+                            <div class="btn-group2 d-flex align-items-center justify-content-between">
+                                <div>
+                                    <p class="text muted">The Important Of Art Pictures, Try It. This weekend was a
+                                        bit different, though. </p>
+                                    <a class="text-dark user" href="https://medium.com/">veccilin</a>
+                                    <span class="text-dark user">in</span>
+                                    <a class="text-dark user" href="https://medium.com/">PaintLove</a>
+                                    <br><time class=" text-muted user">a few hours
+                                        ago</time><span class="text-muted user"> <img width="6px"
+                                            src="images/primitive-dot (2).svg" alt=""> 3 min
+                                        read </span><span> <img width="8px" src="images/star (3).svg" alt="">
+                                    </span>
+                                </div>
+                                <div class="d-flex mr-2">
+                                    <img class="menu" width="23px" src="images/guardar1.svg" alt="guardar">
+                                    <img class="menu" width="20px" src="images/more_horiz-24px (1).svg" alt="dotts">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`)
+            })
+        },
+        dataType: "json",
+        cache: false
+      });
+    } 
 
 
 $(window).on("load", getPost);
-
+let currentDate = new Date()
 function getPost () {
   $.ajax({
     url: 'https://ajaxclass-1ca34.firebaseio.com/medium-equipo1/posts/.json',
     success: function(respuesta) {
 
         for(let key in respuesta){
-            let postRecent = respuesta[key]
+            let postRecent = respuesta[key]           
 
-            console.log(postRecent)
+            
+            if(postRecent.category === "Coronavirus"){                
+                printLeftCardRecent(postRecent)
+            }else if(postRecent.category ==="OneZero"){
+                printRightCards(postRecent)
+            }
+            else{
+                printCenterCards(postRecent)
+            }
         }
 
     }
 })
 } 
+const printLeftCardRecent =(post)=>{
+    $("#cardLeftPost").append(`<div class="card-mb-3">
+    <div class="row no-gutters d-flex">
+        <div class="col-mb-4">
+            <img class="imageess"
+                src="${post.imageurl}"
+                alt="atardecer">
+        </div>
+        <div class="col-8 ml-4">
+            <div class="textos">
+                <p class="card-title font-weight-bold">${post.title}</p>
+                <p class="text-card">${post.description}
+                </p>
+            </div>
+            <div class="row">
+                <div class="col-9 p-0">
+                    <a class="text-dark user ml-3" href="https://medium.com/">${post.author}</a>
+                    <span class="text-dark user">in </span>
+                    <a class="text-dark user" href="https://medium.com/">${post.category}</a>
+                    <br><time class=" text-muted user ml-3">${post.date}</time><span class="text-muted small"> <img width="6px"
+                            src="images/primitive-dot (2).svg" alt="doot"> 3 min
+                        read </span><span> <img width="8px" src="images/star (3).svg" alt=""> </span>
+                </div>
+                <div class="col-3 p-0 d-flex justify-content-end">
+                    <img class="d-md-none" width="23px" src="images/guardar1.svg" alt="guardar">
 
+                    <img class="mr-2 menu" width="23px" src="images/more_horiz-24px (1).svg" alt="dotts">
+                </div>
+            </div>
+
+        </div>
+    </div>`)
+}
+const printCenterCards =(post)=>{
+    $("#cardsCenter").append(`<div id="second-card" class="card mb-3">
+    <div class="row no-gutters d-flex align-items-center  flex-row-reverse flex-md-row">
+        <div class="col-4">
+            <img width="100%"
+                src="${post.imageurl}"
+                class="card-img mb-3" alt="">
+        </div>
+        <div class="col-8">
+            <div class="card-body pr-0 pt-0">
+                <h5 class="card-title font-weight-bold">${post.title}</h5>
+                <div class="btn-group2 d-flex align-items-center justify-content-between">
+                    <div>
+                        <a class="text-dark user" href="https://medium.com/">${post.author}</a>
+                        <span class="text-dark user">in</span>
+                        <a class="text-dark user" href="https://medium.com/">${post.category}</a>
+                        <br><time class=" text-muted user">${post.date}</time><span class="text-muted user"> <img width="6px"
+                                src="images/primitive-dot (2).svg" alt=""> 3 min
+                            read </span><span> <img width="8px" src="images/star (3).svg" alt="">
+                        </span>
+                    </div>
+                    <div>
+                        <img class="d-md-none" width="23px" src="images/guardar1.svg" alt="guardar">
+                        <img class="dotts m-0" src="images/more_horiz-24px (1).svg" alt="dotts">
+                    </div>
+                </div>
+            </div>
+        </div>       
+`)
+}
+const printRightCards =(post)=>{
+    $("#rightCard").append(`<div class="card-mb-3">
+    <div class="row no-gutters">
+        <div class="col-mb-4">
+            <img class="imageess"
+                src="${post.imageurl}"
+                alt="atardecer 2">
+        </div>
+        <div class="col-md-8 ml-4">
+            <div class="textos">
+                <p class="card-title font-weight-bold">${post.title}</p>
+                <p class="text-card">${post.description}</p>
+            </div>
+            <a class="text-dark user" href="https://medium.com/">${post.author}</a><span
+                class="text-dark small">
+                in </span>
+            <a class="text-dark user" href="https://medium.com/">${post.category}</a>
+            <br><time class=" text-muted user">${post.date}</time><span
+                class="text-muted small"> <img width="6px" src="images/primitive-dot (2).svg"
+                    alt="doot"> 3 min
+                read </span><span> <img width="8px" src="images/star (3).svg" alt=""> </span>
+            <div class="btn-group">
+                <img class="d-md-none" width="23px" src="images/guardar1.svg" alt="guardar">
+                <img width="23px" src="images/more_horiz-24px (1).svg" alt="dotts">
+            </div>
+        </div>
+    </div>
+</div>`)
+}
